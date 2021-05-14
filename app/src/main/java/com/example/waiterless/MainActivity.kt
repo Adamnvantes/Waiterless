1d9a655b72a41f21bb59fdf7e294f2817d344e51
Adam <Adamnvantes@gmail.com>
2021-05-14 15:12:16 -0700

    Deleted login activity. Made main activity login page. Added elements to Main Activity

--- app/src/main/java/com/example/waiterless/MainActivity.kt
+++ app/src/main/java/com/example/waiterless/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.waiterless
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Variable declarations
+        var userInputEmail = findViewById<TextView>(R.id.mainInputEmail)
+        var userInputPassword = findViewById<TextView>(R.id.mainInputPassword)
+
+        //Setting hints for user input
+        userInputEmail.hint = "Please enter your email address"
+        userInputPassword.hint = "Please enter your password"
     }
 }
