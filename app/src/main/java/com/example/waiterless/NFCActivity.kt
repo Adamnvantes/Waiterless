3bf5ea1f151be9079c86c0e65ced320319cf4406
JustinBush <justin.bush3210@yahoo.com>
2021-05-14 20:23:39 -0700

    for the NFC, and customer home activities

---
+++ app/src/main/java/com/example/waiterless/NFCActivity.kt
@@ -0,0 +1,17 @@
+package com.example.waiterless
+
+//Justin
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class NFCActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nfc)
+
+
+    }
+
+}
--- app/src/main/java/com/example/waiterless/NFCactivity.kt
+++
@@ -1,4 +0,0 @@
-package com.example.waiterless
-
-class NFCactivity {
-}
