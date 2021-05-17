56ea519c2c16e1a7430fb552e90c837ed81959ef
JustinBush <justin.bush3210@yahoo.com>
2021-05-17 14:28:18 -0700

    customer home activity and its layout made

--- app/src/main/java/com/example/waiterless/activities/RestaurantListActivity.kt
+++ app/src/main/java/com/example/waiterless/activities/RestaurantListActivity.kt
@@ -1,6 +1,8 @@
 package com.example.waiterless.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.example.waiterless.R
 
@@ -12,6 +14,17 @@ class RestaurantListActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant_list)
 
+        val papaMarioPizzaBTN = findViewById<Button>(R.id.papaMarioPizzaButton)
+        papaMarioPizzaBTN.setOnClickListener {
+            val intent = Intent( this, RestaurantActivity::class.java)  // for now
+            startActivity(intent)
+        }
+
+        val buffetShackBTN = findViewById<Button>(R.id.buffetShackButton)
+        buffetShackBTN.setOnClickListener {
+            val intent = Intent( this, RestaurantActivity::class.java)  // for now
+            startActivity(intent)
+        }
 
     }
 }
