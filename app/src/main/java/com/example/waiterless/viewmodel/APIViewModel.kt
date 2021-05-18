0af93b38b0d636a8111f81aece9dc7101fcd14b2
Adam <Adamnvantes@gmail.com>
2021-05-17 18:15:37 -0700

    Added API calls to ViewModel

--- app/src/main/java/com/example/waiterless/viewmodel/APIViewModel.kt
+++ app/src/main/java/com/example/waiterless/viewmodel/APIViewModel.kt
@@ -4,18 +4,17 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.waiterless.models.FoodModel
-import com.example.waiterless.models.RestaurantModel
 import com.example.waiterless.repository.Repository
 import kotlinx.coroutines.launch
 
 class APIViewModel(private val repository: Repository): ViewModel() {
-    val restaurantModelResponse : MutableLiveData<Array<FoodModel>> = MutableLiveData()
+    val menuResponse : MutableLiveData<Array<FoodModel>> = MutableLiveData()
     val stringResponse : MutableLiveData<String> = MutableLiveData()
 
     fun getMenu(r_id : Int){
         viewModelScope.launch {
-            val response : RestaurantModel = repository.getMenu(r_id)
-            restaurantModelResponse.value = response
+            val response : Array<FoodModel> = repository.getMenu(r_id)
+            menuResponse.value = response
         }
     }
 
@@ -29,6 +28,41 @@ class APIViewModel(private val repository: Repository): ViewModel() {
     fun sendOrder(r_id : Int, table: Int, f_id : Int){
         viewModelScope.launch {
             val response : String = repository.sendService(r_id, table)
+            stringResponse.value = response
+        }
+    }
+
+    fun checkUser(utype : String, email : String){
+        viewModelScope.launch {
+            val response : String = repository.checkUser(utype, email)
+            stringResponse.value = response
+        }
+    }
+
+    fun addUser(utype: String, name : String, birthday : String, email : String, password : String){
+        viewModelScope.launch {
+            val response : String = repository.addUser(utype, name, birthday, email, password)
+            stringResponse.value = response
+        }
+    }
+
+    fun editUser(utype : String, email : String, part : String, value : String){
+        viewModelScope.launch {
+            val response : String = repository.editUser(utype, email, part, value)
+            stringResponse.value = response
+        }
+    }
+
+    fun deleteUser(utype : String, email : String){
+        viewModelScope.launch {
+            val response : String = repository.deleteUser(utype, email)
+            stringResponse.value = response
+        }
+    }
+
+    fun checkPass(utype: String, email: String, password: String){
+        viewModelScope.launch {
+            val response : String = repository.checkPass(utype, email, password)
             stringResponse.value = response
         }
     }
