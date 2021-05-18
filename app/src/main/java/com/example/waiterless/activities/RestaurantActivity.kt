b360118ca153dc509266364613c958874e913916
JustinBush <justin.bush3210@yahoo.com>
2021-05-18 14:54:55 -0700

    customer home activity and its layout made

--- app/src/main/java/com/example/waiterless/activities/RestaurantActivity.kt
+++ app/src/main/java/com/example/waiterless/activities/RestaurantActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import com.example.waiterless.R
 
 //food orders
-class RestaurantActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class RestaurantActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_restaurant)
 
-//pull id num
-        //ex: id num = 0, now spit out entire menu for a particular restaurant
+        //pull id num
+        //ex: id num = 0, now display entire menu for Papa Mario's Pizza
+
+
     }
 }
