f0e511e920975e9def42d78c06de7a70857f416f
Adam <Adamnvantes@gmail.com>
2021-05-17 22:02:15 -0700

    Added guest login functionality. Added constants

--- app/src/main/java/com/example/waiterless/activities/MainActivity.kt
+++ app/src/main/java/com/example/waiterless/activities/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.waiterless.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.waiterless.R
+import com.example.waiterless.objects.Constants
 import com.example.waiterless.repository.Repository
 import com.example.waiterless.viewmodel.APIViewModel
 import com.example.waiterless.viewmodel.APIViewModelFactory
@@ -28,31 +28,29 @@ class MainActivity : AppCompatActivity() {
         val btnRegister = findViewById<Button>(R.id.mainBtnRegister)
         val btnGuest = findViewById<Button>(R.id.mainBtnGuest)
         val btnEmployee = findViewById<Button>(R.id.mainBtnEmployee)
-        var makeaccount = false
 
         //API variables
         val repository = Repository()
         val viewModelFactory = APIViewModelFactory(repository)
 
         //Setting hints for user input
-        userInputEmail.hint = "Please enter your email address"
-        userInputPassword.hint = "Please enter your password"
+        userInputEmail.hint = Constants.ENTEREMAIL
+        userInputPassword.hint = Constants.ENTERPASS
 
         //Grabbing information
         btnLogin.setOnClickListener {
             val username = userInputEmail.text.toString()
             val password = userInputPassword.text.toString()
-            makeaccount = false
 
             viewModel = ViewModelProvider(this, viewModelFactory).get(APIViewModel::class.java)
-            viewModel.checkPass("customers", username, password)
+            viewModel.checkPass(Constants.CUSTOMERTAG, username, password)
             viewModel.stringResponse.observe(this, Observer { response ->
-                if (response == "200") {
+                if (response == Constants.OKCODE) {
                     var intent = Intent(this, CustomerHomeActivity::class.java)
-                    intent.putExtra("email", username)
+                    intent.putExtra(Constants.INTENTEXTRATAG, username)
                     startActivity(intent)
                 } else {
-                    Toast.makeText(this, "Please Try Again!", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, Constants.TRYAGAIN, Toast.LENGTH_SHORT).show()
                 }
             })
         }
@@ -62,26 +60,26 @@ class MainActivity : AppCompatActivity() {
             val password = userInputPassword.text.toString()
 
             viewModel = ViewModelProvider(this, viewModelFactory).get(APIViewModel::class.java)
-            viewModel.addUser("customers", "-1", "-1", username, password)
+            viewModel.addUser(Constants.CUSTOMERTAG, Constants.DEFAULTNAME, Constants.DEFAULTBIRTHDAY, username, password)
             viewModel.stringResponse.observe(this, Observer { response ->
                 when (response) {
-                    "200" -> {
-                        Toast.makeText(this, "Success! Log in", Toast.LENGTH_SHORT).show()
+                    Constants.OKCODE -> {
+                        Toast.makeText(this, Constants.REGISTERSUCCESS, Toast.LENGTH_SHORT).show()
                     }
-                    "400" -> {
-                        Toast.makeText(this, "Username already exists", Toast.LENGTH_SHORT).show()
+                    Constants.CONFLICTCODE -> {
+                        Toast.makeText(this, Constants.REGISTERCONFLICT, Toast.LENGTH_SHORT).show()
                     }
                     else -> {
-                        Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(this, Constants.ERROR, Toast.LENGTH_SHORT).show()
                     }
                 }
             })
+        }
 
-
-            btnGuest.setOnClickListener {
-                //val intent = Intent
-            }
-
+        btnGuest.setOnClickListener {
+            val intent = Intent(this, CustomerHomeActivity::class.java)
+            intent.putExtra(Constants.INTENTEXTRATAG, Constants.GUEST)
+            startActivity(intent)
         }
     }
 }
