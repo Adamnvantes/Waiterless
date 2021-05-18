7dd7a5f24eaf188421740c4b59afa181421f9af6
Adam <Adamnvantes@gmail.com>
2021-05-17 18:04:08 -0700

    Added API function calls to repository

--- app/src/main/java/com/example/waiterless/api/WaiterlessAPI.kt
+++ app/src/main/java/com/example/waiterless/api/WaiterlessAPI.kt
@@ -1,17 +1,33 @@
 package com.example.waiterless.api
 
+import com.example.waiterless.models.FoodModel
 import com.example.waiterless.models.RestaurantModel
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface WaiterlessAPI {
 
-    @GET("v1/{r_id}/menu")
-    suspend fun getMenu(@Path("r_id") r_id : Int) : RestaurantModel
+    @GET("v1/menu/{r_id}")
+    suspend fun getMenu(@Path("r_id") r_id : Int) : Array<FoodModel>
 
-    @GET("v1/{r_id}/service/{table}")
+    @GET("v1/service/{r_id}/{table}")
     suspend fun sendService(@Path("r_id") r_id : Int, @Path("table") table : Int) : String
 
-    @GET("v1/{r_id}/order/{table}/{f_id}")
+    @GET("v1/order/{r_id}/{table}/{f_id}")
     suspend fun sendOrder(@Path("r_id") r_id : Int, @Path("table") table : Int, @Path("f_id") f_id : Int) : String
+
+    @GET("v1/check/{utype}/{email}")
+    suspend fun checkUser(@Path("utype") utype : String, @Path("email") email : String) : String
+
+    @GET("v1/add/{utype}/{name}/{birthday}/{email}/{password}")
+    suspend fun addUser(@Path("utype") utype: String, @Path("name") name : String, @Path("birthday") birthday : String, @Path("email") email : String, @Path("password") password : String) : String
+
+    @GET("v1/edit/{utype}/{email}/{part}/{value}")
+    suspend fun editUser(@Path("utype") utype : String, @Path("email") email : String, @Path("part") part : String, @Path("value") value : String) : String
+
+    @GET("v1/delete/{utype}/{email}")
+    suspend fun deleteUser(@Path("utype") utype : String, @Path("email") email : String) : String
+
+    @GET("v1/checkpass/{utype}/{email}/{password}")
+    suspend fun checkPass(@Path("utype") utype : String, @Path("email") email : String, @Path("password") password : String) : String
 }
--- app/src/main/java/com/example/waiterless/viewmodel/APIViewModel.kt
+++ app/src/main/java/com/example/waiterless/viewmodel/APIViewModel.kt
@@ -3,12 +3,13 @@ package com.example.waiterless.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.waiterless.models.FoodModel
 import com.example.waiterless.models.RestaurantModel
 import com.example.waiterless.repository.Repository
 import kotlinx.coroutines.launch
 
 class APIViewModel(private val repository: Repository): ViewModel() {
-    val restaurantModelResponse : MutableLiveData<RestaurantModel> = MutableLiveData()
+    val restaurantModelResponse : MutableLiveData<Array<FoodModel>> = MutableLiveData()
     val stringResponse : MutableLiveData<String> = MutableLiveData()
 
     fun getMenu(r_id : Int){
