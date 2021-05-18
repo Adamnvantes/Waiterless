29938db7b7ec4873b5821de1b8b102f18d584007
Adam <Adamnvantes@gmail.com>
2021-05-18 15:00:17 -0700

    Added Keys to git

---
+++ app/src/main/java/com/example/waiterless/api/Keys.kt
@@ -0,0 +1,10 @@
+package com.example.waiterless.api
+
+object Keys {
+    var APPID = "1204716"
+    var KEY = "fa5a261d11b91d27a570"
+    var SECRET = "f61103dcd4152ba9bced"
+    var CLUSTER = "us3"
+    var SSL = true
+    var MARIOCHANNEL = "mario-channel"
+}
