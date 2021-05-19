83f6b3a58181dbb6b79dc599f3f370de824c039f
JustinBush <justin.bush3210@yahoo.com>
2021-05-18 20:52:36 -0700

    restaurant activity and its layout made

--- app/src/main/java/com/example/waiterless/activities/RestaurantActivity.kt
+++ app/src/main/java/com/example/waiterless/activities/RestaurantActivity.kt
@@ -39,60 +39,60 @@ class RestaurantActivity : AppCompatActivity() {
                 fun run(menu: Array<FoodModel>){
                     //TODO: Update menu here
                     //item 1
-                    val menuItem1Pic = findViewById<ImageView>(R.id.menuItem1Pic)
+                    val menuItem1Pic = findViewById<ImageView>(R.id.menuItem1Pic)  // for the first menu item's picture
                     Picasso.get().load(menu[0].link).into(menuItem1Pic)
-                    val food1Txt = findViewById<TextView>(R.id.foodText1)
+                    val food1Txt = findViewById<TextView>(R.id.foodText1) // for the menu's first food item
                     food1Txt.text = menu[0].name
-                    val price1Txt = findViewById<TextView>(R.id.priceText1)
+                    val price1Txt = findViewById<TextView>(R.id.priceText1) // for the first item's price
                     price1Txt.text = menu[0].price.toString()
 
                     //item 2
-                    val menuItem2Pic = findViewById<ImageView>(R.id.menuItem2Pic)
-                    Picasso.get().load(menu[0].link).into(menuItem2Pic)
-                    val food2Txt = findViewById<TextView>(R.id.foodText2)
-                    food2Txt.text = menu[0].name
-                    val price2Txt = findViewById<TextView>(R.id.priceText2)
-                    price2Txt.text = menu[0].price.toString()
+                    val menuItem2Pic = findViewById<ImageView>(R.id.menuItem2Pic) // for the second menu item's picture
+                    Picasso.get().load(menu[1].link).into(menuItem2Pic)
+                    val food2Txt = findViewById<TextView>(R.id.foodText2) // for the menu's second food item
+                    food2Txt.text = menu[1].name
+                    val price2Txt = findViewById<TextView>(R.id.priceText2) // for the second item's price
+                    price2Txt.text = menu[1].price.toString()
 
                     //item 3
-                    val menuItem3Pic = findViewById<ImageView>(R.id.menuItem3Pic)
-                    Picasso.get().load(menu[0].link).into(menuItem3Pic)
-                    val food3Txt = findViewById<TextView>(R.id.foodText3)
-                    food3Txt.text = menu[0].name
-                    val price3Txt = findViewById<TextView>(R.id.priceText3)
-                    price3Txt.text = menu[0].price.toString()
+                    val menuItem3Pic = findViewById<ImageView>(R.id.menuItem3Pic) // for the third menu item's picture
+                    Picasso.get().load(menu[2].link).into(menuItem3Pic)
+                    val food3Txt = findViewById<TextView>(R.id.foodText3) // for the menu's third food item
+                    food3Txt.text = menu[2].name
+                    val price3Txt = findViewById<TextView>(R.id.priceText3) // for the third item's price
+                    price3Txt.text = menu[2].price.toString()
 
                     //item 4
-                    val menuItem4Pic = findViewById<ImageView>(R.id.menuItem4Pic)
-                    Picasso.get().load(menu[0].link).into(menuItem4Pic)
-                    val food4Txt = findViewById<TextView>(R.id.foodText4)
-                    food4Txt.text = menu[0].name
-                    val price4Txt = findViewById<TextView>(R.id.priceText4)
-                    price4Txt.text = menu[0].price.toString()
+                    val menuItem4Pic = findViewById<ImageView>(R.id.menuItem4Pic) // for the fourth menu item's picture
+                    Picasso.get().load(menu[3].link).into(menuItem4Pic)
+                    val food4Txt = findViewById<TextView>(R.id.foodText4) // for the menu's fourth food item
+                    food4Txt.text = menu[3].name
+                    val price4Txt = findViewById<TextView>(R.id.priceText4) // for the fourth item's price
+                    price4Txt.text = menu[3].price.toString()
 
                     //item 5
-                    val menuItem5Pic = findViewById<ImageView>(R.id.menuItem5Pic)
-                    Picasso.get().load(menu[0].link).into(menuItem5Pic)
-                    val food5Txt = findViewById<TextView>(R.id.foodText5)
-                    food5Txt.text = menu[0].name
-                    val price5Txt = findViewById<TextView>(R.id.priceText5)
-                    price5Txt.text = menu[0].price.toString()
+                    val menuItem5Pic = findViewById<ImageView>(R.id.menuItem5Pic) // for the fifth menu item's picture
+                    Picasso.get().load(menu[4].link).into(menuItem5Pic)
+                    val food5Txt = findViewById<TextView>(R.id.foodText5) // for the menu's fifth food item
+                    food5Txt.text = menu[4].name
+                    val price5Txt = findViewById<TextView>(R.id.priceText5) // for the fifth item's price
+                    price5Txt.text = menu[4].price.toString()
 
                     //item 6
-                    val menuItem6Pic = findViewById<ImageView>(R.id.menuItem6Pic)
-                    Picasso.get().load(menu[0].link).into(menuItem6Pic)
-                    val food6Txt = findViewById<TextView>(R.id.foodText6)
-                    food6Txt.text = menu[0].name
-                    val price6Txt = findViewById<TextView>(R.id.priceText6)
-                    price6Txt.text = menu[0].price.toString()
+                    val menuItem6Pic = findViewById<ImageView>(R.id.menuItem6Pic) // for the sixth menu item's picture
+                    Picasso.get().load(menu[5].link).into(menuItem6Pic)
+                    val food6Txt = findViewById<TextView>(R.id.foodText6) // for the menu's sixth food item
+                    food6Txt.text = menu[5].name
+                    val price6Txt = findViewById<TextView>(R.id.priceText6) // for the sixth item's price
+                    price6Txt.text = menu[5].price.toString()
 
                     //item 7
-                    val menuItem7Pic = findViewById<ImageView>(R.id.menuItem7Pic)
-                    Picasso.get().load(menu[0].link).into(menuItem7Pic)
-                    val food7Txt = findViewById<TextView>(R.id.foodText7)
-                    food7Txt.text = menu[0].name
-                    val price7Txt = findViewById<TextView>(R.id.priceText7)
-                    price7Txt.text = menu[0].price.toString()
+                    val menuItem7Pic = findViewById<ImageView>(R.id.menuItem7Pic) // for the seventh menu item's picture
+                    Picasso.get().load(menu[6].link).into(menuItem7Pic)
+                    val food7Txt = findViewById<TextView>(R.id.foodText7) // for the menu's seventh food item
+                    food7Txt.text = menu[6].name
+                    val price7Txt = findViewById<TextView>(R.id.priceText7) // for the seventh item's price
+                    price7Txt.text = menu[6].price.toString()
 
                 }
 
