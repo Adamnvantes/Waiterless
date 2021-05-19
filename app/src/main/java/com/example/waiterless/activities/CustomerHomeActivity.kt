42c73ee06370258bfd9dd37bec349582858af2ad
Adam <Adamnvantes@gmail.com>
2021-05-18 20:11:22 -0700

    Updated buttons for restaurant choice

--- app/src/main/java/com/example/waiterless/activities/CustomerHomeActivity.kt
+++ app/src/main/java/com/example/waiterless/activities/CustomerHomeActivity.kt
@@ -70,6 +70,22 @@ class CustomerHomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedList
             spinnerRestaurantAll.adapter = aa
         }
 
+        //Button for all restaurants choice
+        val btnAll = findViewById<Button>(R.id.btnRestaurantAll)
+        btnAll.setOnClickListener {
+            val intent = Intent(this, RestaurantActivity::class.java)
+            intent.putExtra("rID",spinnerPos)
+            startActivity(intent)
+        }
+
+        //Button for all restaurants choice
+        val btnRecent = findViewById<Button>(R.id.btnRestaurantRecent)
+        btnAll.setOnClickListener {
+            val intent = Intent(this, RestaurantActivity::class.java)
+            intent.putExtra("rID",spinnerPos)
+            startActivity(intent)
+        }
+
         spinnerEdit.prompt = "Value"
         spinnerEdit.onItemSelectedListener = this
         spinnerEdit.gravity = Gravity.CENTER
@@ -80,7 +96,7 @@ class CustomerHomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedList
 
         //Button sets new value
         btnSubmit.setOnClickListener {
-            if(editInfo.text.equals("")){
+            if(editInfo.text.isEmpty()){
                 Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
             } else{
                 var part = "name"
@@ -110,35 +126,6 @@ class CustomerHomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedList
 
 
             }
-        }
-
-
-        //Variable holding restaurant ID
-        var rID = 0
-
-        //option to select restaurants
-        //1. Find from a list (Papa Mario's Pizza, and other made-up restaurants)
-        val restaurantListBTN = findViewById<Button>(R.id.restaurantListButton)
-        restaurantListBTN.setOnClickListener {
-            val intent = Intent( this, RestaurantListActivity::class.java)
-            intent.getIntExtra("rID", rID)
-            startActivity(intent)
-        }
-
-        //2. Scan a QR code
-        val qrCodeBTN = findViewById<Button>(R.id.qrCodeButton)
-        qrCodeBTN.setOnClickListener {
-            val intent = Intent( this, QRcodeActivity::class.java)
-            intent.getIntExtra("rID", rID)
-            startActivity(intent)
-        }
-
-        //3. Select a previously selected restaurant (from recent history)
-        val recentHistoryBTN = findViewById<Button>(R.id.recentHistoryButton)
-        recentHistoryBTN.setOnClickListener {
-            val intent = Intent( this, RestaurantActivity::class.java)  // for now
-            intent.getIntExtra("rID", rID)
-             startActivity(intent)
         }
 
     }
